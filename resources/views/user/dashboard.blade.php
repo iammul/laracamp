4c424ef064a35750f287a85fcd0ad42000cf96b6
mulia619 <muliakunn@gmail.com>
2022-09-06 02:41:27 +0800

    integration landing page, checkout, and dashboard

---
+++ resources/views/user/dashboard.blade.php
@@ -0,0 +1,61 @@
+@extends('layouts.app')
+
+@section('content')
+
+<section class="dashboard my-5">
+    <div class="container">
+        <div class="row text-left">
+            <div class=" col-lg-12 col-12 header-wrap mt-4">
+                <p class="story">
+                    DASHBOARD
+                </p>
+                <h2 class="primary-header ">
+                    My Bootcamps
+                </h2>
+            </div>
+        </div>
+        <div class="row my-5">
+            <table class="table">
+                <tbody>
+                    @forelse ($checkouts as $item)
+                        <tr class="align-middle">
+                            <td width="18%">
+                                <img src="{{ asset('images/item_bootcamp.png') }}" height="120" alt="">
+                            </td>
+                            <td>
+                                <p class="mb-2">
+                                    <strong>{{ $item->Camp->title }}</strong>
+                                </p>
+                                <p>
+                                    {{ $item->created_at->format('M d, Y') }}
+                                </p>
+                            </td>
+                            <td>
+                                <strong>{{ $item->Camp->price }}K</strong>
+                            </td>
+                            <td>
+                                @if ($item->is_paid)
+                                    <strong class="text-success">Success Payment</strong>
+                                @else
+                                    <strong>Waiting for Payment</strong>
+                                @endif
+
+                            </td>
+                            <td>
+                                <a href="https://wa.me/082158445894?text=Hi, saya ingin bertanya tentang kelas {{ $item->Camp->title }}" class="btn btn-primary">
+                                    Contact Support
+                                </a>
+                            </td>
+                        </tr>
+                    @empty
+                        <tr>
+                            <td colspan="5">No Data</td>
+                        </tr>
+                    @endforelse
+                </tbody>
+            </table>
+        </div>
+    </div>
+</section>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\User\CheckoutController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,19 +20,33 @@ Route::get('/', function () {
     return view('welcome');
 })->name('welcome');
 
-Route::get('/checkout', function () {
-    return view('checkout');
-})->name('checkout');
+Route::middleware(['auth'])->group(function () {
+    //dashboard routes
+    Route::get('dashboard', [HomeController::class, 'dashboard'])->name(
+        'dashboard'
+    );
 
-Route::get('/success-checkout', function () {
-    return view('success-checkout');
-})->name('success-checkout');
+    //checkout routes
+    Route::get('checkout/success', [
+        CheckoutController::class,
+        'success',
+    ])->name('checkout.success');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})
-    ->middleware(['auth'])
-    ->name('dashboard');
+    Route::get('checkout/{camp:slug}', [
+        CheckoutController::class,
+        'create',
+    ])->name('checkout.create');
+
+    Route::post('checkout/{camp}', [CheckoutController::class, 'store'])->name(
+        'checkout.store'
+    );
+});
+
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })
+//     ->middleware(['auth'])
+//     ->name('dashboard');
 
 //socialite routes
 Route::get('sign-in-google', [UserController::class, 'google'])->name(
